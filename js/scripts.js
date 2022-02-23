e31882f717930dbe48800a8619cdf2d04620efdd
Bruno Assis <bhmassisbhmassis@gmail.com>
2022-02-23 11:57:39 -0300

    Adição de funções

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,32 @@ $(()=>{
         $('.text-area').toggleClass('underline');
         $('#sublinhado').toggleClass('selected');
     })
-
+    
+    $('#direita').click(()=>{
+        $('.text-area').css('text-align', 'right');
+        $('#direita').toggleClass('selected');
+        $('#centro').removeClass('selected');
+        $('#esquerda').removeClass('selected');
+    })
+    
+    $('#centro').click(()=>{
+        $('.text-area').css('text-align', 'center');
+        $('#centro').toggleClass('selected');
+        $('#direita').removeClass('selected');
+        $('#esquerda').removeClass('selected');
+    })
+    
+    $('#esquerda').click(()=>{
+        $('.text-area').css('text-align', 'left');
+        $('#esquerda').toggleClass('selected');
+        $('#centro').removeClass('selected');
+        $('#direita').removeClass('selected');
+    })
+    
+    
+    $('#font-size').change(()=>{
+        var tamanho = $('#font-size').val() + 'px';
+        $('.text-area').css('font-size', tamanho);
+    })
     
 });
